28e777231b3fb6acbdb783cc4cc5f5c7026a508b
Alex Vasilkov <alexvasilkov@gmail.com>
2016-12-03 12:50:29 +0700

    Added position animation state to debug overlay

--- library/src/main/java/com/alexvasilkov/gestures/internal/DebugOverlay.java
+++ library/src/main/java/com/alexvasilkov/gestures/internal/DebugOverlay.java
@@ -7,7 +7,7 @@ import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.graphics.RectF;
-import android.util.TypedValue;
+import android.graphics.Typeface;
 import android.view.View;
 
 import com.alexvasilkov.gestures.GestureController;
@@ -19,6 +19,7 @@ import com.alexvasilkov.gestures.views.interfaces.AnimatorView;
 import com.alexvasilkov.gestures.views.interfaces.GestureView;
 
 import java.lang.reflect.Field;
+import java.util.Locale;
 
 public class DebugOverlay {
 
@@ -39,65 +40,93 @@ public class DebugOverlay {
         final ViewPositionAnimator animator = ((AnimatorView) view).getPositionAnimator();
         final Settings settings = controller.getSettings();
         final Context context = view.getContext();
-        final int top = view.getPaddingTop();
-        final int left = view.getPaddingLeft();
         final float stroke = UnitsUtils.toPixels(context, STROKE_WIDTH);
+        final float textSize = UnitsUtils.toPixels(context, TEXT_SIZE);
 
-        paint.setStyle(Paint.Style.STROKE);
-        paint.setStrokeWidth(stroke);
+        canvas.save();
+        canvas.translate(view.getPaddingLeft(), view.getPaddingTop());
 
         // Viewport
         rectF.set(0f, 0f, settings.getViewportW(), settings.getViewportH());
-        drawRect(canvas, Color.GRAY, top, left);
+        drawRect(canvas, rectF, Color.GRAY, stroke);
 
         // Movement area
         GravityUtils.getMovementAreaPosition(settings, rect);
         rectF.set(rect);
-        drawRect(canvas, Color.GREEN, top, left);
+        drawRect(canvas, rectF, Color.GREEN, stroke);
 
         // Image bounds with rotation
-        canvas.save();
-        canvas.translate(left, top);
         controller.getState().get(matrix);
+        canvas.save();
         canvas.concat(matrix);
         rectF.set(0f, 0f, settings.getImageW(), settings.getImageH());
-        paint.setStrokeWidth(stroke / controller.getState().getZoom());
-        drawRect(canvas, Color.YELLOW, 0, 0);
-        paint.setStrokeWidth(stroke);
+        drawRect(canvas, rectF, Color.YELLOW, stroke / controller.getState().getZoom());
         canvas.restore();
 
         // Image bounds
         rectF.set(0f, 0f, settings.getImageW(), settings.getImageH());
         controller.getState().get(matrix);
         matrix.mapRect(rectF);
-        drawRect(canvas, Color.RED, top, left);
+        drawRect(canvas, rectF, Color.RED, stroke);
 
         // State source
         float pos = animator.getPositionState();
         if (pos == 1f || (pos == 0f && animator.isLeaving())) {
-            paint.setTextSize(UnitsUtils.toPixels(context, TEXT_SIZE));
-            paint.setTextAlign(Paint.Align.CENTER);
-            paint.setStyle(Paint.Style.FILL);
-            paint.setColor(Color.CYAN);
-            float dx = settings.getViewportW() * 0.5f + left;
-            float dy = settings.getViewportH() * 0.5f + top;
+            final StateSource source = getStateSource(controller);
 
-            StateSource source = getStateSource(controller);
-            canvas.drawText(source.name(), dx, dy, paint);
+            drawText(canvas, settings, source.name(), Color.CYAN, textSize);
 
             if (source != StateSource.NONE) {
                 view.invalidate();
             }
+        } else if (pos > 0f) {
+            String direction = animator.isLeaving() ? "EXIT" : "ENTER";
+            String text = String.format(Locale.US, "%s %.0f%%", direction, pos * 100f);
+            drawText(canvas, settings, text, Color.MAGENTA, textSize);
         }
+
+        canvas.restore();
     }
 
-    private static void drawRect(Canvas canvas, int color, int top, int left) {
-        float strokeHalf = paint.getStrokeWidth() * 0.5f;
-        rectF.offset(left, top);
-        rectF.inset(strokeHalf, strokeHalf);
+    private static void drawRect(Canvas canvas, RectF rect, int color, float stroke) {
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeWidth(stroke);
+        rectF.inset(0.5f * stroke, 0.5f * stroke);
         paint.setColor(color);
-        canvas.drawRect(rectF, paint);
+        canvas.drawRect(rect, paint);
     }
+
+    private static void drawText(Canvas canvas, Settings settings, String text,
+            int color, float textSize) {
+        // Text settings
+        paint.setTextSize(textSize);
+        paint.setTypeface(Typeface.MONOSPACE);
+        paint.setTextAlign(Paint.Align.CENTER);
+
+        final float halfSize = 0.5f * textSize;
+
+        // Computing text background
+        paint.getTextBounds(text, 0, text.length(), rect);
+        rectF.set(rect);
+        rectF.offset(-rectF.centerX(), -rectF.centerY());
+        GravityUtils.getMovementAreaPosition(settings, rect);
+        rectF.offset(rect.centerX(), rect.centerY());
+        rectF.inset(-halfSize, -halfSize);
+
+        // Drawing background
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.WHITE);
+        canvas.drawRoundRect(rectF, halfSize, halfSize, paint);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.GRAY);
+        canvas.drawRoundRect(rectF, halfSize, halfSize, paint);
+
+        // Drawing text
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(color);
+        canvas.drawText(text, rectF.centerX(), rectF.bottom - halfSize, paint);
+    }
+
 
     private static StateSource getStateSource(GestureController controller) {
         // We can't have public API for state source in GestureController,
